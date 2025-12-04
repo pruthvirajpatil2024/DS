d346c88c1cbed753571b787b843f25bfdb20f418
Pruthviraj Patil <137247110+pruthvirajpatil2024@users.noreply.github.com>
2025-12-05 05:02:46 +0530

    Implement remove_nth_occurrence function
    Add a function to remove the nth occurrence of a word from a list.

--- Python7.py
+++ Python7.py
@@ -1,1 +1,24 @@
+def remove_nth_occurrence(lst, word, n):
+    count = 0
+
+    for i in range(len(lst)):
+        if lst[i] == word:
+            count += 1
+
+            if count == n:
+                del lst[i]
+                return lst
+
+    return lst  # if occurrence not found
+
+
+# Driver Code
+words = ["apple", "banana", "apple", "orange", "apple"]
+print("Original List=", words)
+
+word = "banana"
+n = 1
+
+result = remove_nth_occurrence(words, word, n)
+print(f"after removing (n) th occurrence OF '{word}':", result)
 
