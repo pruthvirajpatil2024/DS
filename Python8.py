303efab6586e605c0ae104ad183cf66487d02e36
Pruthviraj Patil <137247110+pruthvirajpatil2024@users.noreply.github.com>
2025-12-05 04:51:17 +0530

    Remove word frequency counting logic

--- Python8.py
+++ Python8.py
@@ -1,11 +1,1 @@
-text="python is easy and  python is powerfull "
-words=text.split()
-word_freq={}
-for word in words:
-    if word in word_freq:
-        word_freq[word]+=1
-    else:
-        word_freq[word]=1
-print("word frequency in the string")
-for word,freq in word_freq.items():
-    print(f"{word}:{freq}")
+
