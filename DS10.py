9f4adb59aad3acac56deb72c2d634a0582b55d2d
Pruthviraj Patil <137247110+pruthvirajpatil2024@users.noreply.github.com>
2025-12-05 04:17:54 +0530

    Create DS10.py to demonstrate dictionary mapping

---
+++ DS10.py
@@ -0,0 +1,9 @@
+keys = ["name", "age", "city"]
+values = ["Alice", 25, "Pune"]
+
+print("Keys List:", keys)
+print("Values List:", values)
+
+# Mapping using zip() function
+mapped_dict = dict(zip(keys, values))
+print("Mapped Dictionary:", mapped_dict)
